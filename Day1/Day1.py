2c259ceefc7e61d65175dfe8035c9fb1f39f8f0a
Bjoergermeister <bjarnesvensson@web.de>
2021-09-19 12:33:54 +0200

    Day 1 Puzzle 2

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -7,8 +7,22 @@ def Puzzle1(input):
 
     return totalFuel
 
+def Puzzle2(input):
+    totalFuel = 0
+    for line in input:
+        currentFuel = int(line)
+        while True:
+            currentFuel = math.floor(currentFuel / 3) - 2
+            if currentFuel <= 0:
+                break
+
+            totalFuel += currentFuel
+            
+    return totalFuel
+
 if __name__ == "__main__":
     inputFile = open("input.txt", "r")
     input = inputFile.readlines()
 
     print("Puzzle 1: " + str(Puzzle1(input)))
+    print("Puzzle 2: " + str(Puzzle2(input)))
