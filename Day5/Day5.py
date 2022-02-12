9477b6b34c53eb8063259d8f7b8a8af623019ece
Bjoergermeister <bjarnesvensson@web.de>
2022-02-12 17:30:35 +0100

    Day 5 Puzzle 2

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -1,4 +1,6 @@
-def run(intcode):
+import copy
+  
+def run(intcode, input):
 
     index = 0
     while(intcode[index] != 99):
@@ -23,18 +25,49 @@ def run(intcode):
             index += 4
         if (mode == "03"):
             position = index + 1 if is_immediate_mode3 else intcode[index + 1]
-            intcode[position] = 1
+            intcode[position] = input
             index += 2
         if (mode == "04"):
             output = intcode[index + 1] if is_immediate_mode3 else intcode[intcode[index + 1]] 
             index += 2
+        if (mode == "05"):
+            operand1 = intcode[index + 1] if is_immediate_mode1 else intcode[intcode[index + 1]]
+            operand2 = intcode[index + 2] if is_immediate_mode2 else intcode[intcode[index + 2]]
+            if operand1 != 0:
+                index = operand2
+            else:
+                index += 3
+        if (mode == "06"):
+            operand1 = intcode[index + 1] if is_immediate_mode1 else intcode[intcode[index + 1]]
+            operand2 = intcode[index + 2] if is_immediate_mode2 else intcode[intcode[index + 2]]
+            if operand1 == 0:
+                index = operand2
+            else:
+                index += 3
+        if (mode == "07"):
+            operand1 = intcode[index + 1] if is_immediate_mode1 else intcode[intcode[index + 1]]
+            operand2 = intcode[index + 2] if is_immediate_mode2 else intcode[intcode[index + 2]]
+            position = index + 3 if is_immediate_mode3 else intcode[index + 3]
+            intcode[position] = 1 if operand1 < operand2 else 0
+            index += 4
+        if (mode == "08"):
+            operand1 = intcode[index + 1] if is_immediate_mode1 else intcode[intcode[index + 1]]
+            operand2 = intcode[index + 2] if is_immediate_mode2 else intcode[intcode[index + 2]]
+            position = index + 3 if is_immediate_mode3 else intcode[index + 3]
+            intcode[position] = 1 if operand1 == operand2 else 0
+            index += 4
 
     return output
 
-def Puzzle1(intcode):    
-    return run(intcode)
+def Puzzle1(intcode, input):    
+    return run(intcode, input)
+
+def Puzzle2(intcode, input):
+    return run(intcode, input)
 
 if __name__ == "__main__":
     input = open("input.txt", "r")
     numbers = [int(x) for x in input.readline().split(",")]
-    print("Puzzle 1: " + str(Puzzle1(numbers)))
+    numbersCopy = copy.deepcopy(numbers)
+    print("Puzzle 1: " + str(Puzzle1(numbers, 1)))
+    print("Puzzle 2: " + str(Puzzle2(numbersCopy, 5)))
