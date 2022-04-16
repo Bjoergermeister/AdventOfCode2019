f422d1d37c92063e95a0747124ad312af0065284
Bjoergermeister <bjarnesvensson@web.de>
2022-04-16 18:07:45 +0200

    Day 9 Puzzle 1

---
+++ Day9/Day9.py
@@ -0,0 +1,62 @@
+relative_base = 0
+
+def get_index(intcode, index, mode):
+    if mode == '0':
+        return intcode[index]
+    if mode == '1':
+        return index
+    if mode == '2':
+        return intcode[index] + relative_base
+
+def run(intcode, eingabe):
+    global relative_base
+
+    index = 0
+    while(intcode[index] != 99):
+        instruction = str(intcode[index]).zfill(2)
+        
+        mode = instruction[-2:]
+        operand1_mode = instruction[len(instruction) - 3] if len(instruction) > 2 else '0'
+        operand2_mode = instruction[len(instruction) - 4] if len(instruction) > 3 else '0'
+        operand3_mode = instruction[len(instruction) - 5] if len(instruction) > 4 else '0'
+
+        index1 = get_index(intcode, index + 1, operand1_mode)
+        index2 = get_index(intcode, index + 2, operand2_mode)
+        index3 = get_index(intcode, index + 3, operand3_mode)
+
+        operand1 = intcode[index1] if index1 in intcode else 0
+        operand2 = intcode[index2] if index2 in intcode else 0
+
+        if (mode == "01"):
+            intcode[index3] = operand1 + operand2
+            index += 4
+        if (mode == "02"):
+            intcode[index3] = operand1 * operand2
+            index += 4
+        if (mode == "03"):
+            intcode[index1] = eingabe 
+            index += 2
+        if (mode == "04"):
+            output = intcode[index1]
+            index += 2
+        if (mode == "05"):
+            index = operand2 if operand1 != 0 else index + 3
+        if (mode == "06"):
+            index = operand2 if operand1 == 0 else index + 3 
+        if (mode == "07"):
+            intcode[index3] = 1 if operand1 < operand2 else 0
+            index += 4
+        if (mode == "08"):
+            intcode[index3] = 1 if operand1 == operand2 else 0
+            index += 4
+        if (mode == "09"):
+            relative_base += intcode[index1]
+            index += 2
+    return output
+
+if __name__ == "__main__":
+    input = open("input.txt", "r")
+    intcode = {}
+    for index, number in enumerate(input.readline().split(",")):
+        intcode[index] = int(number)
+    print("Puzzle 1: " + str(run(intcode, 1)))
