daf72a1ed7291139886646640e61c3f414d65808
Bjoergermeister <bjarnesvensson@web.de>
2022-09-11 00:10:33 +0200

    Day 7 Puzzle 2

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -3,10 +3,11 @@ from itertools import permutations
 import copy
 
   
-def run(intcode, phase, input):
+def run(loop, intcode, phase, input, pc = None):
 
-    index = 0
-    is_first_input = True
+    output = 0
+    index = 0 if loop == False else pc
+    is_first_input = True if loop == False else pc == 0
     while(intcode[index] != 99):
         instruction = str(intcode[index]).zfill(2)
         
@@ -14,7 +15,8 @@ def run(intcode, phase, input):
         is_immediate_mode1 = len(instruction) > 2 and instruction[len(instruction) - 3] == '1'
         is_immediate_mode2 = len(instruction) > 3 and instruction[len(instruction) - 4] == '1'
         is_immediate_mode3 = len(instruction) > 4 and instruction[len(instruction) - 5] == '1'
-        
+
+
         if (mode == "01"):
             operand1 = intcode[index + 1] if is_immediate_mode1 else intcode[intcode[index + 1]]
             operand2 = intcode[index + 2] if is_immediate_mode2 else intcode[intcode[index + 2]]
@@ -38,6 +40,8 @@ def run(intcode, phase, input):
         if (mode == "04"):
             output = intcode[index + 1] if is_immediate_mode3 else intcode[intcode[index + 1]] 
             index += 2
+            if loop: # If feedback loop mode is on, return here so the next amplifier can continue running. Return the output and the current programm counter
+                return output, index, False
         if (mode == "05"):
             operand1 = intcode[index + 1] if is_immediate_mode1 else intcode[intcode[index + 1]]
             operand2 = intcode[index + 2] if is_immediate_mode2 else intcode[intcode[index + 2]]
@@ -65,24 +69,58 @@ def run(intcode, phase, input):
             intcode[position] = 1 if operand1 == operand2 else 0
             index += 4
 
-    return output
+    if loop:
+        return output, index, True
+    else:
+        return output
 
-def Puzzle1(intcode, input):
+def Puzzle1(intcode):
     phases = list(permutations(range(0, 5)))
     input = 0
     highest_output = -1
     for phase in phases:    
         intcodeCopy = copy.deepcopy(intcode)
-        output = run(intcodeCopy, phase[0], input)
-        output = run(intcodeCopy, phase[1], output)
-        output = run(intcodeCopy, phase[2], output)
-        output = run(intcodeCopy, phase[3], output)
-        output = run(intcodeCopy, phase[4], output)
+        output = run(False, intcodeCopy, phase[0], input)
+        output = run(False, intcodeCopy, phase[1], output)
+        output = run(False, intcodeCopy, phase[2], output)
+        output = run(False, intcodeCopy, phase[3], output)
+        output = run(False, intcodeCopy, phase[4], output)
         if highest_output == -1 or output > highest_output:
             highest_output = output
+    return highest_output
+
+def Puzzle2(intcode):
+    phases = list(permutations(range(5, 10)))
+    output = 0
+    highest_output = -1
+    for phase in phases:
+        state = [
+            { 'intcode': copy.deepcopy(intcode), 'phase_setting': phase[0], 'input': 0, 'pc': 0 },
+            { 'intcode': copy.deepcopy(intcode), 'phase_setting': phase[1], 'input': 0, 'pc': 0 },
+            { 'intcode': copy.deepcopy(intcode), 'phase_setting': phase[2], 'input': 0, 'pc': 0 },
+            { 'intcode': copy.deepcopy(intcode), 'phase_setting': phase[3], 'input': 0, 'pc': 0 },
+            { 'intcode': copy.deepcopy(intcode), 'phase_setting': phase[4], 'input': 0, 'pc': 0 }
+        ]
+
+        while True:
+            is_finished = False
+            for i in range(0, 5):
+                output, pc, is_finished = run(True, state[i]['intcode'], state[i]['phase_setting'], state[i]['input'], state[i]['pc'])
+                if is_finished == False:
+                    state[i]['pc'] = pc
+                    index = i + 1 if i < 4 else 0
+                    state[index]['input'] = output
+
+            if is_finished:
+                break
+
+        if highest_output == -1 or state[0]['input'] > highest_output:
+            highest_output = state[0]['input']
+
     return highest_output
 
 if __name__ == "__main__":
     input = open("input.txt", "r")
     numbers = [int(x) for x in input.readline().split(",")]
-    print("Puzzle 1: " + str(Puzzle1(numbers, 1)))
+    print("Puzzle 1: " + str(Puzzle1(numbers)))
+    print("Puzzle 2: " + str(Puzzle2(numbers)))
