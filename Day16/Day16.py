c38d2bd001f25c61c8bbecd4016728ae70922c59
Bjoergermeister <bjarnesvensson@web.de>
2023-01-31 23:52:15 +0100

    Day 16 Puzzle 2

--- Day16/Day16.py
+++ Day16/Day16.py
@@ -1,30 +1,43 @@
-def build_patterns(pattern_count):
-    base_pattern = [0, 1, 0, -1]
-    patterns = []
-
-    for i in range(1, pattern_count + 1):
-        next_pattern = []
-        for j in range(len(base_pattern)):
-            next_pattern.extend([base_pattern[j]] * i)
-        
-        patterns.append(next_pattern)
-    return patterns
-
 def Puzzle1(input):
-    patterns = build_patterns(len(input))
-
     current_input = input
     for phase in range(0, 100):
-        new_input = [0] * len(input)
         for i in range(0, len(input)):
-            new_input[i] = abs(sum([value * patterns[i][(index + 1) % len(patterns[i])] for index, value in enumerate(current_input)])) % 10
+            pattern_length = i * 4 + 4
 
-        current_input = new_input
+            total = 0
+            pattern_offset = 0
+            while pattern_offset < len(input):
+                pattern_section_size = pattern_length // 4
+                positive_begin = pattern_offset + (pattern_section_size - 1)
+                positive_end = pattern_offset + (pattern_section_size * 2 - 1)
+                negative_begin = pattern_offset + (pattern_section_size * 3 - 1)
+                negative_end = pattern_offset + (pattern_section_size * 4 - 1)
+                
+                sum_positive = sum(current_input[positive_begin:positive_end])
+                sum_negative = sum(current_input[negative_begin:negative_end])
+
+                total += sum_positive
+                total -= sum_negative
+
+                pattern_offset += pattern_length
+            current_input[i] = abs(total) % 10
     
     return "".join(map(lambda number: str(number), current_input[0:8]))
 
-
+def Puzzle2(long_list):
+    offset = 0
+    for i in range(0, 7):
+        offset = (offset * 10) + long_list[i]
+    
+    for phase in range(0, 100):
+        for i in reversed(range(offset, len(long_list) - 1)):
+            long_list[i] = (long_list[i] + long_list[i + 1]) % 10                
+    return ''.join(map(lambda x: str(x), long_list[offset:offset+8])) # Convert list of number to string
+        
 if __name__ == '__main__':
     input = open("input.txt", "r")
     list = [int(number) for number in input.readline()]
+    long_list = list.copy() * 10000
+
     print(f"Puzzle 1: {Puzzle1(list)}")
+    print(f"Puzzle 2: {Puzzle2(long_list)}")
